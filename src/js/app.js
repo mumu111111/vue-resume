5b74783325b473981412ab07fbe51e9329f8edeb
mumu111111 <2646709035@qq.com>
2018-04-09 19:24:11 +0800

    init

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+var app= new Vue({
+    el: '#app',
+    data:{
+        editingName: false,
+        resume:{
+            name: '姓名',
+            gender: '女',
+            birthday: '1994年4月',
+            jobTitle: '前端工程师',
+            phone: '13266666666',
+            email: 'example@example.com'
+        }
+    },
+    methods: {
+        onEdit(key,value){//修改的value放到resume中
+            this.resume[key] = value
+        },
+    }
+})
---
+++ src/js/av.js
@@ -0,0 +1,7 @@
+var APP_ID = 'xOawLzIySKiyNynO4wGFbUJf-gzGzoHsz';
+var APP_KEY = 'J8tNWguuWzyWksqIMCx6dpw3';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
