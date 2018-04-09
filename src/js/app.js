080e9cc09b8b47d3e4ddacd242c17a3d84235f7c
mumu111111 <2646709035@qq.com>
2018-04-09 22:09:20 +0800

    添加注册\登录\登出功能

--- src/js/app.js
+++ src/js/app.js
@@ -11,11 +11,42 @@ let app= new Vue({
             jobTitle: '前端工程师',
             phone: '13266666666',
             email: 'example@example.com'
+        },
+        login:{
+            email: '',
+            password: ''
+        },
+        signUp:{
+            email: '',
+            password: ''
         }
+    
     },
     methods: {
         onEdit(key,value){//修改的value放到resume中
             this.resume[key] = value
+        },
+        onLogin(e){
+            AV.User.logIn(this.login.email,this.login.password)
+            .then(function(user){
+                console.log(user)
+            }, function(error){
+                if(error.code=== 211){
+                    alert('邮箱不存在')
+                }else if(error.code=== 210){
+                    alert('邮箱或密码不匹配')
+                }
+            })
+        },
+        onSignUp(e){
+            const user = new AV.User()
+            user.setUsername(this.signUp.email)
+            user.setPassword(this.signUp.password)
+            user.setEmail(this.signUp.email)
+            user.signUp().then(function (user){
+                console.log(user)
+            }, function(error){
+            })
         },
         onClickSave(){
             let currentUser = AV.User.current()
@@ -25,8 +56,17 @@ let app= new Vue({
                 this.saveResume()
             }
         },
+        onLogOut(){
+            AV.User.logOut()
+            alert('注销成功')
+            window.location.reload()
+        },
         saveResume(){
+            let {id}= AV.User.current()
+            let user= AV.Object.createWithoutData('User', id)
+            user.set('resume', this.resume)
+            user.save()
+        },
 
-        }       
     }
 })
